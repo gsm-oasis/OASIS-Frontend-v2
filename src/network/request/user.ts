0f4241577553c5db194a9d4c816daa0b0166aa3b
ParkSeoJune <tjwns4122@gmail.com>
2023-08-21 09:47:40 +0900

    user api 설정

---
+++ src/network/request/user.ts
@@ -0,0 +1,96 @@
+import { getUser } from "../data/getUrl";
+import { instance } from "../data/instance";
+
+class User {
+  linkCouple(coupleCode: string) {
+    try {
+      return instance({
+        method: "POST",
+        url: getUser.linkCouple(),
+        data: {
+          code: coupleCode,
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  setBirthday(birthday: string) {
+    try {
+      return instance({
+        method: "POST",
+        url: getUser.setBirthday(),
+        params: {
+          anniversaryDate: birthday,
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  getInfo() {
+    try {
+      return instance({
+        method: "GET",
+        url: getUser.getInfo(),
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  userWithdrawal() {
+    try {
+      return instance({
+        method: "DELETE",
+        url: getUser.userWithdrawal(),
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  cutOffCouple() {
+    try {
+      return instance({
+        method: "DELETE",
+        url: getUser.cutOffCouple(),
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  changePassword(originalPassword: string, password: string) {
+    try {
+      return instance({
+        method: "PATCH",
+        url: getUser.changePassword(),
+        data: {
+          originalPassword: originalPassword,
+          password: password,
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  changeNickname(nickname: string) {
+    try {
+      return instance({
+        method: "PATCH",
+        url: getUser.changeNickname(),
+        data: {
+          nickname: nickname,
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+export default new User();
