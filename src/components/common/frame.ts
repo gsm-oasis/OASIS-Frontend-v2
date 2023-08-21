24c53a0e3a8d04950430a2d16d0658033ae02646
ParkSeoJune <tjwns4122@gmail.com>
2023-08-21 10:06:10 +0900

    frame 설정

---
+++ src/components/common/frame.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Setting = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Frame = styled.div`
+  width: 40vw;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+`;
+
+export const ContentFrame = styled.div`
+  width: 375px;
+`;
