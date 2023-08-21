7604d54e01fdf568bc9053321030e3a37c29c032
ParkSeoJune <tjwns4122@gmail.com>
2023-08-21 17:14:10 +0900

    토스트 오류 해결

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import GlobalStyle from "@/styles/globals";
 import type { AppProps } from "next/app";
-import { RecoilRoot, useRecoilState } from "recoil";
+import { RecoilRoot } from "recoil";
+import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
