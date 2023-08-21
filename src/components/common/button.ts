6782d955f426bb4e7c93ac9b521dd0f1f3acfe26
ParkSeoJune <tjwns4122@gmail.com>
2023-08-21 17:14:24 +0900

    공통 버튼 스타일 추가

--- src/components/common/button.ts
+++ src/components/common/button.ts
@@ -13,3 +13,19 @@ export const GradiantButton = styled.button`
   font-weight: 700;
   cursor: pointer;
 `;
+
+export const CheckButton = styled.button<{
+  color: string;
+  weight: string;
+  height: string;
+}>`
+  width: ${(props) => props.weight}px; // 60
+  height: ${(props) => props.height}px; // 50
+  margin-left: 20px;
+  border-style: none;
+  color: #fff;
+  background: #${(props) => props.color}; // f4cacc
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 14px;
+`;
