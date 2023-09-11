9ed90bfb38aeaf84034368a6566bbb7bfd39a6ba
ParkSeoJune <tjwns4122@gmail.com>
2023-09-11 12:14:20 +0900

    빈 파일 제거

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,19 +1,10 @@
 export { Logo, SmallLogo } from "./logo";
-
 export { Back } from "./back";
-
 export { Heart, BlankHeart, RedHeart } from "./heart";
-
 export { SettingIcon } from "./setting";
-
 export { Plus, BoldPlus } from "./plus";
-
 export { DeleteButton } from "./delete";
-
 export { Mail, ColorMail } from "./mail";
-
 export { Next } from "./next";
-
 export { Calender, ColorCalender } from "./calender";
-
 export { Hearts } from "./hearts";
